5e1c19adc44ad9f56acde7dd0548a41fce324676
Rina Mendoza <rinamendozaa@gmail.com>
2022-09-26 16:15:23 -0400

    inserted an object type to the schema

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const { ApolloServer, gql } = require("apollo-server");
 
 //Scaler type is a data type
-//string!]! is say if there is not a string, return null and the second bang is saying do not return null - return nothing.
+//[string!]! is say if there is not a string, return null and the second bang is saying do not return null - return nothing.
 
 const typeDefs = gql`
   type Query {
     hello: [String!]!
-    numberOfAnimals: Int
-    price: Float
-    isCool: Boolean
+    products: [Product!]!
+  }
+  # Object type is [Product]
+  type Product {
+    name: String!
+    description: String!
+    quantity: Int!
+    price: Float!
+    onSale: Boolean
   }
 `;
 
@@ -17,17 +23,35 @@ const resolvers = {
     hello: () => {
       return ["hello", "my", "friend"];
     },
-    numberOfAnimals: () => {
-      return 88;
-    },
-    price: () => {
-      return 99.99;
-    },
-    isCool: () => {
-      return false;
-    },
+    products: () => [
+      {
+        name: "Bike",
+        description: "Mountain Bike",
+        price: "3444",
+        onSale: false,
+      },
+    ],
   },
 };
+
+// in the graphql studio, query products and specify what you want returned.
+// query {
+//     products {
+//       name
+//       price
+//     }
+//     }
+// output =
+// {
+//     "data": {
+//       "products": [
+//         {
+//           "name": "Bike",
+//           "price": 3444
+//         }
+//       ]
+//     }
+//   }
 const server = new ApolloServer({
   typeDefs,
   resolvers,
